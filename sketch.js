c5c1709b9a367889f6666ee2d5b6a13067422c82
SYDNEY COLVIN <SGC21E@FSU.EDU>
2023-09-27 09:19:40 -0400

    Testing Input

--- sketch.js
+++ sketch.js
@@ -1,16 +1,26 @@
 let height;
 let width;
+let pdf1;
+let pdf2;
+let input = false;
+
+function preload() {
+  pdf1 = loadStrings('assets/GoogleTOS.txt');
+  pdf2 = loadStrings('assets/GoogleTOS_2.txt');
+}
 
 function setup() {
   width = windowWidth;
   height = windowHeight;
   createCanvas(width, height);
-  background(225);
-  text("I love Lucy", 50, 50);
+  
 }
 
 function draw() {
-  
+  background(225);
+  text(pdf1, 50, 50, 300, 300);
+  text(pdf2, 50, 320, 300, 300);
+  showInput();
 }
 
 function windowResized() {
@@ -19,6 +29,9 @@ function windowResized() {
   resizeCanvas(width,height);
 }
 
-function displayTOS() {
-  
+function showInput() {
+  if (input === false) {
+    input = createInput("Answer Here");
+    input = true;
+  }
 }
