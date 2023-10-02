f9e83e1fbe558a1677fff7fa04a3724a859952f5
SYDNEY COLVIN <SGC21E@FSU.EDU>
2023-10-02 08:43:35 -0400

    Psuedocode and notes for new method

--- sketch.js
+++ sketch.js
@@ -1,17 +1,26 @@
+let pdfPages = [];
 // #declare array of images
+let questions = [];
 // #declare array of questions (just the text like "What does Google have access to in....")
+let answers = [];
 // #declare array of the correct answers
+
 
 function setup() {
 //     #call function to load the next image and question
 }
 
+function loadNextPage(pdfPages + questions) {
+  
+}
 // function loadImageAndQuestionStuff(parameter of the current image number?) {
 //     # check that we aren't over the bounds of the array
 //     # load the image
 //     # load the radio buttons (maybe call a function to do this for you?)
 // }
-
+function createRadioButtons(pdfPages + questions) {
+  
+}
 // function createRadioButtons(parameter of current image/question number) {
 //     # use the createButton function from p5.js
 //     # create a submit button for the question
