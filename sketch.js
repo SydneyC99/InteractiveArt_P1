f4ae7fb09e2f12462baf15dc74ae98aef94567b7
SYDNEY COLVIN <SGC21E@FSU.EDU>
2023-10-02 11:35:53 -0400

    Got a picture popping up

--- sketch.js
+++ sketch.js
@@ -1,26 +1,38 @@
-let pdfPages = [];
+let pdfQuestions = ["img1", "img2", "img3"];
+let i = 0;
 // #declare array of images
-let questions = [];
+// let questions = ["q1", "q2", "q3"];
 // #declare array of questions (just the text like "What does Google have access to in....")
-let answers = [];
+//let answers = ["a1", "a2", "a3"];
 // #declare array of the correct answers
 
 
 function setup() {
+  noCanvas();
+  let button = createButton("A");
+  button.mouseClicked(loadNextPage);
+
 //     #call function to load the next image and question
 }
 
-function loadNextPage(pdfPages) {
-  
+function loadNextPage() {
+  for (let i = 0; i < 2; i++) {
+    let p =  createP(pdfQuestions[i]);
+    i++;
+    console.log("Got it!");
+    let img = createImg("assets/ladyBug-" + nf(i,2) + ".jpg");
+    img.parent(p);
+  }
+ 
 }
 // function loadNextPage(parameter of the current image number?) {
 //     # check that we aren't over the bounds of the array
 //     # load the image
 //     # load the radio buttons (maybe call a function to do this for you?)
 // }
-function createRadioButtons(pdfPages, questions) {
+// function createRadioButtons(pdfPages, questions) {
   
-}
+// }
 // function createRadioButtons(parameter of current image/question number) {
 //     # use the createButton function from p5.js
 //     # create a submit button for the question
