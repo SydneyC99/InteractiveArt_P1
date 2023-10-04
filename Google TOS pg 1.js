c3ca8bd1d77861373076e75b7ba59b3d91926d45
SYDNEY COLVIN <SGC21E@FSU.EDU>
2023-10-04 10:38:01 -0400

    Pages Swap Properly
    Co-authored-by: Rob Duarte <me@robduarte.com>

--- Google TOS pg 1.js
+++ Google TOS pg 1.js
@@ -1,8 +1,8 @@
-let pdfQuestions = ("Question 1: Who are you contracting with?");
+let pdfQuestions = "Question 1: Who are you contracting with?";
 let answers;
-let A = ("Google LLC");
-let B = ("The Internet");
-let C = ("My Computer");
+let A = "A) Google LLC";
+let B = "B) The Internet";
+let C = "C) My Computer";
 let val;
 // #declare array of images
 // let questions = ["q1", "q2", "q3"];
@@ -13,28 +13,30 @@ let val;
 
 function setup() {
   noCanvas();
-  // let button = createButton("A");
-  // button.mouseClicked(loadNextPage);
+ 
   let p =  createP(pdfQuestions);
   let p2 = createP ();
   console.log("Got it!");
   let img = createImg("assets/ladyBug-00.jpg");
   img.parent(p2);
   answers = createRadio();
-  answers.option ('A)' + " " + A);
-  answers.option ('B)' + " " + B);
-  answers.option ('C)' + " " + C);
-  let val = answers.value();
+  answers.option (A);
+  answers.option (B);
+  answers.option (C);
+  
   // p.mouseClicked(clicked);
   
-//     #call function to load the next image and question
+//     call function to load the next image and question
 }
 
 function mouseClicked () {
-  // if (condition) {
+  if (answers.value() == B || answers.value() == C) {
     location.href = "page2.html";
+    console.log("click!");
+    console.log(answers.value());
+  // } else if (answers.value() == A){
+  //   //load next pdf page
   // }
-    
+
   
-  console.log("click!");
 }
--- wrongAnswer1.js
+++ wrongAnswer1.js
@@ -5,18 +5,26 @@ function setup() {
     p = createP("404 Error");
     p.position(300, 300);
     alert("Nope that's not it! Try again.");
+    
 }
 
-function draw() {
-    let millisecond = millis();
-    if (millisecond = 10000) {
-        location.href = "index.html"
-    }
-}
-
-// function timePassed(){
+// function draw() {
 //     let millisecond = millis();
 //     if (millisecond = 10000) {
 //         location.href = "index.html"
 //     }
 // }
+//How to return to the last page after 10 seconds
+//millis counts the time since everything started, so it doesn't
+//seem to work?
+//Draw stops working over time
+
+//if (10+ seconds have passed since the page has loaded)
+//go back to the other page
+
+function draw(){
+    if (frameCount >= 60*5) {
+        location.href = "index.html";
+    }
+}
+//how to properly call this?
