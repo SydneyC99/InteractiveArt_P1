657c24baf6f9f69593e367dafd2b3386de8b96ad
SYDNEY COLVIN <SGC21E@FSU.EDU>
2023-10-04 09:31:34 -0400

    To Next Page
    Co-authored-by: Rob Duarte <me@robduarte.com>

--- Google TOS pg 1.js
+++ Google TOS pg 1.js
@@ -1,7 +1,9 @@
 let pdfQuestions = ("Question 1: Who are you contracting with?");
+let answers;
 let A = ("Google LLC");
 let B = ("The Internet");
 let C = ("My Computer");
+let val;
 // #declare array of images
 // let questions = ["q1", "q2", "q3"];
 // #declare array of questions (just the text like "What does Google have access to in....")
@@ -22,28 +24,17 @@ function setup() {
   answers.option ('A)' + " " + A);
   answers.option ('B)' + " " + B);
   answers.option ('C)' + " " + C);
+  let val = answers.value();
+  // p.mouseClicked(clicked);
   
 //     #call function to load the next image and question
 }
 
-// function createRadioButtons() {
- 
-
-// }
-
-
-// function loadNextPage(parameter of the current image number?) {
-//     # check that we aren't over the bounds of the array
-//     # load the image
-//     # load the radio buttons (maybe call a function to do this for you?)
-// }
-// function createRadioButtons(pdfPages, questions) {
+function mouseClicked () {
+  // if (condition) {
+    location.href = "page2.html";
+  // }
+    
   
-// }
-// function createRadioButtons(parameter of current image/question number) {
-//     # use the createButton function from p5.js
-//     # create a submit button for the question
-//     # set the mousePressed to check if the answer was right
-//     # by referring to the answer array we declared at the beginning
-//     # if it's right, call loadNextPage
-// }
+  console.log("click!");
+}
--- wrongAnswer1.js
+++ wrongAnswer1.js
@@ -3,6 +3,20 @@ let p;
 
 function setup() {
     p = createP("404 Error");
-    p.position(displayWidth/2, displayHeight/2);
-    p.mouseClicked(fxn)
+    p.position(300, 300);
+    alert("Nope that's not it! Try again.");
 }
+
+function draw() {
+    let millisecond = millis();
+    if (millisecond = 10000) {
+        location.href = "index.html"
+    }
+}
+
+// function timePassed(){
+//     let millisecond = millis();
+//     if (millisecond = 10000) {
+//         location.href = "index.html"
+//     }
+// }
