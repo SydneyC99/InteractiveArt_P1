75dcca06e8c64fad2f945653da27317448a5fa10
SYDNEY COLVIN <SGC21E@FSU.EDU>
2023-10-07 11:03:57 -0400

    Testing Next page

--- Google TOS pg 3.js
+++ Google TOS pg 3.js
@@ -1,8 +1,8 @@
-let pdfQuestions = "Question 2: Who are you contracting with?";
+let pdfQuestions = "Question 3: How can you export your content from your Google Account";
 let answers;
-let A = "A) The Internet";
-let B = "B) Google LLC";
-let C = "C) My Computer";
+let A = "A) The Export Button in Google Apps";
+let B = "B) Copy and Paste";
+let C = "C) Using Google Takeout";
 let val;
 
 function setup() {
@@ -11,7 +11,7 @@ function setup() {
   let p =  createP(pdfQuestions);
   
   console.log("Got it!");
-  let img = createImg("assets/Page_02.jpg");
+  let img = createImg("assets/Page_03.jpg");
   img.parent(p2);
   answers = createRadio();
   answers.option (A);
@@ -22,14 +22,14 @@ function setup() {
 }
 
 function mouseClicked () {
-  if (answers.value() == A || answers.value() == C) {
+  if (answers.value() == A || answers.value() == B) {
     location.href = "wrongInput2.html";
     console.log("click!");
     console.log(answers.value());
   } 
-    else if (answers.value() == B){
-    location.href = "page3.html";
-  }
+//     else if (answers.value() == C){
+//     location.href = "page4.html";
+//   }
 
   
 }
