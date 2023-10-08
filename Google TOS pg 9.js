ff73b7e74ce2d597e6789390f2658e1efe4d5edc
SYDNEY COLVIN <SGC21E@FSU.EDU>
2023-10-08 11:38:23 -0400

    Page 9 Done

--- Google TOS pg 9.js
+++ Google TOS pg 9.js
@@ -1,8 +1,8 @@
-let pdfQuestions = "Question 8: If you are representing an organization, who must agree to these terms?";
+let pdfQuestions = "Question 9: Which of these is true of this license?";
 let answers;
-let A = "A) Anyone who has ever been related to the organization.";
-let B = "B) Only the active user.";
-let C = "C) An authorized organization representative.";
+let A = "A) This license allows you to use Google's branding anywhere at anytime.";
+let B = "B) This license is only valid within the United States.";
+let C = "C) This license is non-assignable.";
 let val;
 
 function setup() {
@@ -11,7 +11,7 @@ function setup() {
   let p =  createP(pdfQuestions);
   
   console.log("Got it!");
-  let img = createImg("assets/Page_08.jpg");
+  let img = createImg("assets/Page_09.jpg");
   img.parent(p2);
   answers = createRadio();
   answers.option (A);
@@ -23,13 +23,13 @@ function setup() {
 
 function mouseClicked () {
   if (answers.value() == B || answers.value() == A) {
-    location.href = "wrongInput8.html";
+    location.href = "wrongInput9.html";
     console.log("click!");
     console.log(answers.value());
   } 
-    else if (answers.value() == C){
-    location.href = "page9.html";
-  }
+//     else if (answers.value() == C){
+//     location.href = "page10.html";
+//   }
 
   
 }
